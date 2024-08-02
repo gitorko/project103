421cc04c703ee9732e213e4ecd76d58e1a7071e8
Arjun Surendra <asurendra@vmware.com>
2024-08-02 14:04:04 +0530

    code

--- src/main/resources/db/changelog/changes/001-create-schema.sql
+++ src/main/resources/db/changelog/changes/001-create-schema.sql
@@ -2,17 +2,5 @@ CREATE TABLE customer (
       id bigserial PRIMARY KEY,
       name varchar(255) UNIQUE NOT NULL,
       description text,
-      tsv tsvector
+      tsv tsvector generated always as ( to_tsvector('english'::regconfig, name || ' ' || coalesce(description, '')) ) stored
 );
-
-CREATE FUNCTION update_customer_tsv() RETURNS trigger AS $$
-BEGIN
-    NEW.tsv := to_tsvector(NEW.name || ' ' || coalesce(NEW.description, ''));
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-    ON customer FOR EACH ROW EXECUTE FUNCTION update_customer_tsv();
-
-CREATE INDEX customer_search_idx ON customer USING gin(tsv);
